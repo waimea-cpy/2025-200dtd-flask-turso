5b76f14eafdde9bf2dc966a0611e692d9684372b
Steve Copley <steve.copley@waimea.school.nz>
2025-06-10 14:34:16 +1200

    Added delete

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask          import Flask
 from flask          import render_template
 from flask          import redirect
+from flask          import request
 from libsql_client  import create_client_sync
 from dotenv         import load_dotenv
 import os
@@ -69,10 +70,45 @@ def new_thing():
 
 
 #-----------------------------------------------------------
+# Process a new thing
+#-----------------------------------------------------------
+@app.post("/add-thing")
+def add_thing():
+    # Get the data from the form
+    name = request.form.get("name")
+    price = request.form.get("price")
+
+    print(name)
+    print(price)
+
+
+    # Connect to the DB
+    client = connect_db()
+
+    # Add the thing to the DB
+    sql = "INSERT INTO things (name, price) VALUES (?, ?)"
+    values = [name, price]
+    client.execute(sql, values)
+
+    # Head to the home page to see the list
+    return redirect("/")
+
+
+
+#-----------------------------------------------------------
 # Thing deletion
 #-----------------------------------------------------------
 @app.get("/delete/<int:id>")
 def delete_thing(id):
+
+    # Connect to the DB
+    client = connect_db()
+
+    # Add the thing to the DB
+    sql = "DELETE FROM things WHERE id=?"
+    values = [id]
+    client.execute(sql, values)
+
     return redirect("/")
 
 
